e83c10eaa987e90fb90282c2232a1ea9fe892d8c
Kolesnikov Andrey <kolesnikovandr@ozon.ru>
2024-01-09 21:19:43 +0300

    feat: добавил расчёт суммы нутриентов

--- src/entities/product/lib.ts
+++ src/entities/product/lib.ts
@@ -1,4 +1,5 @@
 import { IProduct } from './model/types.ts';
+import _ from 'lodash';
 
 export const getProductInitialAmount = (product: IProduct): number => {
   switch (product.type) {
@@ -9,4 +10,26 @@ export const getProductInitialAmount = (product: IProduct): number => {
     default:
       throw new Error('Неизвестный тип продукта');
   }
+};
+
+export const calculateTotalNutrientsAmounts = (
+  product: IProduct,
+  amount: number,
+): {
+  protein: number;
+  fats: number;
+  carbs: number;
+} => {
+  const multiplier = product.type === 'piece' ? amount : amount / 100;
+
+  return _.chain(product)
+    .pick(['protein', 'fats', 'carbs'])
+    .transform(
+      (result, value, key) => {
+        // @ts-expect-error не сходятся типы
+        result[key] = value * multiplier;
+      },
+      { protein: 0, fats: 0, carbs: 0 },
+    )
+    .value();
 };
--- src/widgets/calculator/ui/Calculator/Calculator.tsx
+++ src/widgets/calculator/ui/Calculator/Calculator.tsx
@@ -1,20 +1,112 @@
-import { Card, CardBody } from '@chakra-ui/react';
+import {
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
 import { useProductsAmounts } from '../../model/calculatorStore.ts';
+import { useMemo } from 'react';
+import _ from 'lodash';
+import {
+  calculateTotalNutrientsAmounts,
+  IProduct,
+  useActions,
+} from '@/entities/product';
+import { useCarbs, useFats, useProtein } from '@/features/configureProfile';
 
 export const Calculator = () => {
   const productsAmounts = useProductsAmounts();
+  const protein = useProtein();
+  const fats = useFats();
+  const carbs = useCarbs();
+  const { getProductById } = useActions();
+
+  const preparedData = useMemo(() => {
+    return _.transform(
+      productsAmounts,
+      (result, amount, productId) => {
+        const product = getProductById(productId);
+
+        if (!product) throw new Error('Продукт не найден');
+
+        result[productId] = { product, amount };
+      },
+      {} as Record<string, { product: IProduct; amount: number }>,
+    );
+  }, [productsAmounts]);
+
+  const totalNutrientsAmounts = useMemo(() => {
+    return Object.entries(preparedData).reduce(
+      (acc, [, { product, amount }]) => {
+        const amounts = calculateTotalNutrientsAmounts(product, amount);
+
+        return {
+          protein: acc.protein + amounts.protein,
+          fats: acc.fats + amounts.fats,
+          carbs: acc.carbs + amounts.carbs,
+        };
+      },
+      { protein: 0, fats: 0, carbs: 0 },
+    );
+  }, [preparedData]);
 
   return (
-    <Card>
-      <CardBody>
-        {Object.entries(productsAmounts).map(([productId, amount]) => {
-          return (
-            <div>
-              {productId} * {amount}
-            </div>
-          );
-        })}
-      </CardBody>
-    </Card>
+    <TableContainer overflowY={'auto'}>
+      <Table>
+        <Thead
+          position={'sticky'}
+          top={0}
+          zIndex="docked"
+          backgroundColor={'white'}
+        >
+          <Tr>
+            <Th>Название</Th>
+            <Th isNumeric>Белки</Th>
+            <Th isNumeric>Жиры</Th>
+            <Th isNumeric>Углеводы</Th>
+            <Th isNumeric>Кол-во</Th>
+          </Tr>
+        </Thead>
+
+        <Tbody>
+          {Object.entries(preparedData).map(
+            ([productId, { product, amount }]) => (
+              <Tr key={productId}>
+                <Td>{product.name}</Td>
+                <Td isNumeric>{product.protein}</Td>
+                <Td isNumeric>{product.fats}</Td>
+                <Td isNumeric>{product.carbs}</Td>
+                <Td isNumeric>{amount}</Td>
+              </Tr>
+            ),
+          )}
+        </Tbody>
+
+        <Tfoot
+          position={'sticky'}
+          bottom={0}
+        >
+          <Tr>
+            <Th>Всего:</Th>
+            <Th isNumeric>{totalNutrientsAmounts.protein}</Th>
+            <Th isNumeric>{totalNutrientsAmounts.fats}</Th>
+            <Th isNumeric>{totalNutrientsAmounts.carbs}</Th>
+            <Th></Th>
+          </Tr>
+
+          <Tr>
+            <Th>Цель:</Th>
+            <Th isNumeric>{protein}</Th>
+            <Th isNumeric>{fats}</Th>
+            <Th isNumeric>{carbs}</Th>
+            <Th></Th>
+          </Tr>
+        </Tfoot>
+      </Table>
+    </TableContainer>
   );
 };
