304976013073033d81988c900f911021a779f88b
Kolesnikov Andrey <kolesnikovandr@ozon.ru>
2024-01-12 13:18:12 +0300

    refactor: начал переписывать модель productsPicker на mobx

---
+++ src/widgets/productsPicker/model.ts
@@ -0,0 +1,29 @@
+import { makeAutoObservable } from 'mobx';
+import { productStore, ProductStore } from '@/entities/product';
+import {
+  productsAmountsStore,
+  ProductsAmountsStore,
+} from '@/entities/productAmount';
+
+class ProductsPickerStore {
+  productsStore: ProductStore;
+  productsAmountsStore: ProductsAmountsStore;
+
+  get productsListForPick() {
+    return this.productsStore.products.filter((product) => {
+      const hasProductAmount =
+        !!this.productsAmountsStore.productsAmounts[product.id];
+
+      return !hasProductAmount;
+    });
+  }
+
+  constructor() {
+    makeAutoObservable(this);
+
+    this.productsStore = productStore;
+    this.productsAmountsStore = productsAmountsStore;
+  }
+}
+
+export const productsPickerStore = new ProductsPickerStore();
--- src/widgets/productsPicker/ui/ProductsPicker/ProductsPicker.tsx
+++ src/widgets/productsPicker/ui/ProductsPicker/ProductsPicker.tsx
@@ -1,13 +1,15 @@
-import { ProductRow, productStore } from '@/entities/product';
+import { ProductRow } from '@/entities/product';
 import styles from './ProductsPicker.module.scss';
+import { productsPickerStore } from '../../model.ts';
 import { AddProductInCalculatorButton } from '@/features/addProductInCalculator';
+import { observer } from 'mobx-react-lite';
 
-export const ProductsPicker = () => {
-  const { products } = productStore;
+export const ProductsPicker = observer(() => {
+  const { productsListForPick } = productsPickerStore;
 
   return (
     <div className={styles.picker}>
-      {products.map((product) => (
+      {productsListForPick.map((product) => (
         <ProductRow
           key={product.id}
           product={product}
@@ -22,4 +24,4 @@ export const ProductsPicker = () => {
       ))}
     </div>
   );
-};
+});
